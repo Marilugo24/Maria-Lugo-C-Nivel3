a8521c6e6b50312ce1611d9c9a7fc361bc9a4acc
Maria Lugo <mariafernanda.lugobolivar@gmail.com>
2024-06-15 17:29:46 -0400

    Falta Modificar Codigo

---
+++ Calcular_Area/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string figura;
+double CalcularArea( double Base, double Altura)
+{
+    switch (figura)
+    {
+        case "Rectángulo":
+            return Base * Altura;
+        case "Triángulo":
+            return Base * Altura / 2;
+        case "Círculo":
+            return Math.PI * Base * Base;
+        default:
+            throw new ArgumentException("Figura no válida.");
+    }
+
+}
+
+    Console.WriteLine("Calculadora de areas de figuras, elije una opcion");
+    Console.WriteLine("1. Triangulo");
+    Console.WriteLine("2. Rectangulo");
+    Console.WriteLine("3. Circulo");
+
+    int opcion = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+
+            Console.WriteLine("Ingrese la base y la altura del triangulo:");
+            double BaseTriangulo = Convert.ToDouble(Console.ReadLine());
+            double AlturaTriangulo = Convert.ToDouble(Console.ReadLine());
+            break;
+        case 2:
+            Console.WriteLine("Ingresa la base y la altura del rectángulo:");
+            double BaseRectangulo = Convert.ToDouble(Console.ReadLine());
+            double AlturaRectangulo = Convert.ToDouble(Console.ReadLine());
+            break;
+        case 3:
+            Console.WriteLine("Ingresa el radio del círculo:");
+            double RadioCirculo = Convert.ToDouble(Console.ReadLine());
+            break;
+        default:
+            Console.WriteLine("Opción no válida.");
+            break;
+    }
+double Base = double.Parse(Console.ReadLine());
+
+double AreaCalculada = CalcularArea(Base, Altura);
+Console.WriteLine($"El área del {figura} es: {AreaCalculada}");
+
+
---
+++ Convertidor_Temperatura/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double fahrenheit;
+double celsius;
+double kelvin;
+
+double CelsiusAFahrenheit(double celsius) 
+{
+    return fahrenheit = (9.0 / 5.0) * celsius + 32;
+}
+
+double FahrenheitACelsius(double fahrenheit) 
+{
+    return (5.0 / 9.0) * (fahrenheit - 32);
+}
+
+double CelsiusAKelvin(double celsius) 
+{
+    return celsius + 273.15;
+}
+
+Console.WriteLine("Convertidor de Temperatura, elije la opcion que deseas convertir");
+Console.WriteLine("1. Celsius");
+Console.WriteLine("2. Fahrenheit");
+Console.WriteLine("3. Kelvin");
+
+int opcion = Convert.ToInt32(Console.ReadLine());
+
+switch (opcion)
+{
+    case 1:
+
+        Console.WriteLine("Ingrese la temperatura en Celsius:");
+        double gradoscelcius = Convert.ToDouble(Console.ReadLine());
+       
+        break;
+    case 2:
+        Console.WriteLine("Ingresa la temperatura en Fahrenheit:");
+        double gradosfahrenheit = Convert.ToDouble(Console.ReadLine());
+        break;
+    case 3:
+        Console.WriteLine("Ingrese la temperatura en celcius:");
+        double gradoskelvin = Convert.ToDouble(Console.ReadLine());
+        break;
+    default:
+        Console.WriteLine("Opción no válida.");
+        break;
+}
+
+FahrenheitACelsius(fahrenheit);
+Console.WriteLine($"{fahrenheit} grados Fahrenheit corresponden a {celsius} grados Celsius.");
+
+CelsiusAKelvin(celsius);
+Console.WriteLine($"{celsius} grados Celsius son {kelvin} grados Kelvin.");
+
+CelsiusAFahrenheit(celsius);
+Console.WriteLine($"{celsius} grados Celsius son {fahrenheit} grados fahrenheit.");
